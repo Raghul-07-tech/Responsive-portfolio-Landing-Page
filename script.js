b545b53627ad89ff80c668e65a331d5b6ff55cf7
Raghul-07-tech <krishnamoorthiraghul123@gmail.com>
2026-01-09 15:12:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  const nav = document.getElementById("nav-menu");
+  nav.classList.toggle("active");
+}
+
+// Close menu when a link is clicked (mobile)
+document.querySelectorAll('#nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById('nav-menu').classList.remove('active');
+  });
+});
